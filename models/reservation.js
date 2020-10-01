5c8e67c7ef9e43067877d8c4c1d6b1863ab28eb2
James <jamesseventyseven@gmail.com>
2020-10-02 00:38:03 +0100

    finished

--- models/reservation.js
+++ models/reservation.js
@@ -1,44 +1,59 @@
 /** Reservation for Lunchly */
 
-const moment = require("moment");
+const moment = require('moment');
 
-const db = require("../db");
-
+const db = require('../db');
 
 /** A reservation for a party */
 
 class Reservation {
-  constructor({id, customerId, numGuests, startAt, notes}) {
-    this.id = id;
-    this.customerId = customerId;
-    this.numGuests = numGuests;
-    this.startAt = startAt;
-    this.notes = notes;
-  }
+	constructor({ id, customerId, numGuests, startAt, notes }) {
+		this.id = id;
+		this.customerId = customerId;
+		this.numGuests = numGuests;
+		this.startAt = startAt;
+		this.notes = notes;
+	}
 
-  /** formatter for startAt */
+	/** formatter for startAt */
 
-  getformattedStartAt() {
-    return moment(this.startAt).format('MMMM Do YYYY, h:mm a');
-  }
+	getformattedStartAt() {
+		return moment(this.startAt).format('MMMM Do YYYY, h:mm a');
+	}
 
-  /** given a customer id, find their reservations. */
+	/** given a customer id, find their reservations. */
 
-  static async getReservationsForCustomer(customerId) {
-    const results = await db.query(
-          `SELECT id, 
+	static async getReservationsForCustomer(customerId) {
+		const results = await db.query(
+			`SELECT id, 
            customer_id AS "customerId", 
            num_guests AS "numGuests", 
            start_at AS "startAt", 
            notes AS "notes"
          FROM reservations 
          WHERE customer_id = $1`,
-        [customerId]
-    );
+			[ customerId ]
+		);
 
-    return results.rows.map(row => new Reservation(row));
-  }
+		return results.rows.map((row) => new Reservation(row));
+	}
+
+	async save() {
+		if (this.customerId === undefined) {
+			const err = new Error(`Please select a guest to make a reservation`);
+			err.status = 400;
+			throw err;
+		}
+		const result = await db.query(
+			`INSERT INTO reservations
+        (customer_id, start_at, num_guests, notes)
+        VALUES
+        ($1,$2,$3,$4)
+        RETURNING customer_id, start_at, num_guests, notes`,
+			[ this.customerId, this.startAt, this.numGuests, this.notes ]
+		);
+		return result.rows[0];
+	}
 }
-
 
 module.exports = Reservation;
